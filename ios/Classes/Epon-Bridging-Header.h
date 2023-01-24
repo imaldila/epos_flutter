d8f45f28099b63a3ff166e1bae06c6523eee5658
Aldila <aldilanurhadiputra@gmail.com>
2023-01-25 06:12:54 +0700

    add libs

---
+++ ios/Classes/Epon-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Epon-Bridging-Header.h
+//  epos_flutter
+//
+//  Created by Aldila Nurhadiputra on 25/01/23.
+//
+
+#ifndef Epon_Bridging_Header_h
+#define Epon_Bridging_Header_h
+
+
+#endif /* Epon_Bridging_Header_h */
